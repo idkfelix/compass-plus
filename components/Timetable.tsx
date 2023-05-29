b4604d385ca87d7c3efdade7ab200ce23a469475
idkfelix <felix.coulthard@gmail.com>
2023-05-29 22:52:25 +1000

    final

--- components/Timetable.tsx
+++ components/Timetable.tsx
@@ -20,13 +20,13 @@ export default function Timetable({ data }: { data: Period[] }) {
   function performExtractValues(inputArray: any[]): resultPeriod[] {
     const resultArray: resultPeriod[] = [];
     inputArray.forEach(item => {
-    const sortedString = extractValues(item.topAndBottomLine);
+    const extractedString = extractValues(item.topAndBottomLine);
     const activityId = item.activityId;
     const instanceId = item.instanceId;
     const d = {
       activityId: activityId,
       instanceId: instanceId,
-      data: sortedString
+      data: extractedString
     }
     resultArray.push(d);
     });
@@ -41,9 +41,14 @@ export default function Timetable({ data }: { data: Period[] }) {
         <Link key={period.instanceId} href={`/${period.instanceId}`}>
           <div 
             key={period.instanceId}
-            className='w-[500px] h-[75px] bg-crust rounded-md border-2 border-surface1 m-5 p-3 text-pink'
+            className='w-[500px] h-[75px] bg-crust rounded-md border-2 border-surface1 m-5 p-3 text-pink flex flex-row justify-center'
           >
-            <h1>{period.data[2]}</h1>
+            <div className='flex items-center justify-center'>
+              <h1 className='text-sm text-subtext0 font-semibold'>{period.data[0]}</h1>
+              <h1 className='font-bold'>&nbsp;&nbsp; {period.data[2]}</h1>
+              <h1 className='text-sm text-subtext1 font-semibold'>&nbsp;&nbsp; {period.data[3]}</h1>
+              <h1 className='text-sm text-subtext0 font-bold'>&nbsp;&nbsp; {period.data[4]}</h1>
+            </div>
           </div>
         </Link>
       ))}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '@/components/Footer';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className='frappe'>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <div className='min-h-screen bg-base'>
         <Component {...pageProps} />
         <Footer/>
